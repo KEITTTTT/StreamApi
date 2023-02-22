0fb1084fb7b6d23a6cb9f4c9636e4ad308e466cc
Glebova <e.tulzakova@mail.ru>
2023-02-22 10:19:44 +0400

    Initial commit

---
+++ src/test/java/ru/glebova/streamapi/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glebova.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
