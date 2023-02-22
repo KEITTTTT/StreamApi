e4b254c19cfa1ed9214a142196d511c94d15cf8e
Glebova <e.tulzakova@mail.ru>
2023-02-22 10:49:36 +0400

    Дз сделано

--- src/main/java/ru/glebova/streamapi/Main.java
+++ src/main/java/ru/glebova/streamapi/Main.java
@@ -1,2 +1,31 @@
-package ru.glebova.streamapi;public class Main {
+package ru.glebova.streamapi;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    static List<Integer> list = List.of(5, 8, 12, 31, 32, 40, 2, 54, 16, 7, 1);
+
+    public static void main(String[] args) {
+
+        findMinMax(Stream.of(5, 8, 12, 31, 32, 40, 2, 54, 16, 7, 1), Integer::compareTo,(min, max)->
+        {
+            System.out.printf("min: %d, max: %d \n", min, max);
+        });
+
+        findEvenNumbers(list);
+    }
+
+    public static <T> void findMinMax(Stream<T> stream, Comparator<T> comparator, BiConsumer<T, T> minMaxConsumer) {
+        List<T> list = stream.sorted(comparator).collect(Collectors.toList());
+        minMaxConsumer.accept(list.get(0), list.get(list.size()-1));
+    }
+
+    public static void findEvenNumbers(List<Integer> list) {
+        List<Integer> result = list.stream().filter(e -> e % 2 == 0).toList();
+        System.out.println(result);
+    }
 }
--- src/test/java/ru/glebova/streamapi/StreamApiApplicationTests.java
+++ src/test/java/ru/glebova/streamapi/StreamApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StreamApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
